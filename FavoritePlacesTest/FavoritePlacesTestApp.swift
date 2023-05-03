b9ae3154071995e313e22b27c0802e9c740e41bb
Jacek Kosinski <kashiash@gmail.com>
2023-05-03 23:09:26 +0200

    Initial Commit

---
+++ FavoritePlacesTest/FavoritePlacesTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritePlacesTestApp.swift
+//  FavoritePlacesTest
+//
+//  Created by Jacek Kosiński G on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoritePlacesTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
